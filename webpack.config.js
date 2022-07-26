cd10f1c81dfecb876a34186cd00329cfc48ad311
Dafydd Owen <dowen121@gmail.com>
2022-07-26 09:05:16 +0100

    --hot progress -> react error related to react-devtools?

--- webpack.config.js
+++ webpack.config.js
@@ -1,38 +1,53 @@
 const defaultConfig = require('@wordpress/scripts/config/webpack.config')
 const { getWebpackEntryPoints } = require('@wordpress/scripts/utils/config')
-const path = require('path')
 
 module.exports = {
-  ...defaultConfig,
-  entry: {
-    ...getWebpackEntryPoints(),
-    'app': ['./src/css/app.css', './src/js/app.js'],
-    'editor': ['./src/css/editor.css', './src/js/editor.js'],
-  },
-  devServer: {
-    // watchFiles: ['./**/*.php', './dist/**/*'], // TODO not sure this is doing anything
-    static: {
-      directory: path.join(__dirname, 'dist'),
-    },
-    port: 9000,
-    host: "localhost",
-    devMiddleware: {
-      writeToDisk: true,
-    },
-    proxy: {
-      '/': {
-        publicPath: '/app/themes/pulsar/dist', // TODO dynamic path
-        overlay: true,
-        target: "https://wordpress.test", // TODO dynamic target
-        secure: false,
-        changeOrigin: true,
-        headers: {
-          'Access-Control-Allow-Origin': '*',
-          'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, PATCH, OPTIONS',
-          'Access-Control-Allow-Headers': 'X-Requested-With, content-type, Authorization, text/html, text/css'
-          ,
-        },
-      }
-    },
-  }
+	...defaultConfig,
+	entry: {
+		...getWebpackEntryPoints(),
+		'app-css': ['./src/css/app.css'],
+		'app-js': ['./src/js/app.js'],
+		'editor-css': ['./src/css/app.css'],
+		'editor-js': ['./src/js/editor.js'],
+	},
+	output: {
+		path: __dirname + "/dist",
+		filename: '[name].js',
+		publicPath: "http://localhost:9000/",
+	},
+	mode: 'development',
+	// plugins: [
+	// 	...defaultConfig.plugins,
+	// ],
+	// module: {
+	//   rules: [
+	// 		...defaultConfig.module.rules,
+	//   ],
+	// },
+	devServer: {
+		hot: true,
+		static: __dirname + "/dist/",
+		watchFiles: ['./**/*.php', './src/**/*', './src/app-css.css'], // TODO not sure this is doing anything
+		// devMiddleware: {
+		// 	writeToDisk: true,
+		// },
+		allowedHosts: 'all',
+		host: 'localhost',
+		port: 9000,
+		proxy: {
+			'*': {
+				target: 'https://wordpress.test',
+				secure: false,
+				autoRewrite: true,
+				changeOrigin: true,
+				headers: {
+					'X-ProxiedBy-Webpack': true,
+					'Access-Control-Allow-Origin': '*',
+					'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, PATCH, OPTIONS',
+					'Access-Control-Allow-Headers': 'X-Requested-With, content-type, Authorization, text/html, text/css, text/json'
+					,
+				},
+			},
+		},
+	},
 }
