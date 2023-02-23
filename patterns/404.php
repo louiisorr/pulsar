c47146af64385f872f6e7da80434a4ad5cf8eb8f
Brett Mason <brett@orphans.co.uk>
2023-02-23 12:01:27 +0000

    First look at FSE templates as a viable option

---
+++ patterns/404.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Title: 404 error content.
+ * Slug: pulsar/404
+ * Inserter: false
+ */
+?>
+<!-- wp:heading {"level":1} -->
+<h1 class="wp-block-heading"><?php esc_html_e( "Sorry, this page doesn't exist.", 'pulsar' ); ?></h1>
+<!-- /wp:heading -->
+<!-- wp:paragraph -->
+<p class="wp-block-paragraph"><?php esc_html_e( "It seems we can't find what you're looking for. Perhaps searching can help.", 'pulsar' ); ?></p>
+<!-- /wp:paragraph -->
+<!-- wp:search {"width":75,"widthUnit":"%","showLabel":false,"buttonText":"Search"} /-->
